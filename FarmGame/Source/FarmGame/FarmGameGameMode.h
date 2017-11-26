dc59eb6e3c3bfb18dc50a0c6ef07da8f44c6d7ad
pmurph03 <pmurph.software@gmail.com>
2017-11-25 23:19:05 -0500

    Initial Commit

---
+++ FarmGame/Source/FarmGame/FarmGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "FarmGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FARMGAME_API AFarmGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
