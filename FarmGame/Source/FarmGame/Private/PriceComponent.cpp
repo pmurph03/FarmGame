348973550e5d767ae5be48c76740909303dd10a8
pmurph03 <pmurph.software@gmail.com>
2017-12-14 16:18:06 -0500

    Added a price component for use in future to use for buying / selling components. Also to look into moving some functionality over to actor components instead of blueprints. Made a seperate flowerhead_BP and moved some of the flowering functionality to it instead of the stem or the plant mesh. Associated events / dispatchers added to stems / plants to allow this.

---
+++ FarmGame/Source/FarmGame/Private/PriceComponent.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Plant.h"
+#include "FarmGame.h"
+#include "PriceComponent.h"
+
+
+float UPriceComponent::GetPlayerSellPrice()
+{
+	AActor* Owner = GetOwner();
+	if (Owner != nullptr)
+	{
+		APlant* Plant = Cast<APlant>(Owner);
+		if (Plant != nullptr)
+		{
+			return BasePrice + (BasePrice * Plant->FertilizeCurrent * ScalePriceMultiplier);
+		}
+	}
+	return BasePrice + (BasePrice * (GetOwner()->GetActorScale().X-1) * ScalePriceMultiplier);
+}
+
+float UPriceComponent::GetPlayerBuyPrice()
+{
+	return BasePrice / 1.25;
+}
+
+// Sets default values for this component's properties
+UPriceComponent::UPriceComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPriceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPriceComponent::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
